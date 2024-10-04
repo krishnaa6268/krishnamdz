a64acb4b603a354d412b8475105fb6efdfa20cd1
KRISHNA KUMAR GUPTA <61408269+krishnaa6268@users.noreply.github.com>
2024-10-04 10:27:37 +0530

    Update contactForm.js

--- contactForm.js
+++ contactForm.js
@@ -5,6 +5,6 @@ const scriptURL = 'https://script.google.com/macros/s/AKfycbwPcDAd-qjVzG0en6jpyQ
     e.preventDefault()
     fetch(scriptURL, { method: 'POST', body: new FormData(form)})
       //.then(response => console.log('Success!', response)) //for no msg display background only...
-      .then(alert("The message was sent successfully... we will connect you later, Thank you for your time!!"))
+      .then(alert("The message was sent successfully, we will connect you later... \nThank you for your time !!"))
       .catch(error => console.error('Error!', error.message))
   })
